946c33794aa28dc27e4ffe4dd68f8521599af29e
maheshbasnet089 <basnetmanish089@gmail.com>
2024-10-05 01:31:31 +0545

    add Invoice CRUD

---
+++ Controllers/InvoiceController.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EfCore.Data;
+using EfCore.Models;
+
+namespace Efcore_dotnet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoiceController : ControllerBase
+    {
+        private readonly InvoiceDbContext _context;
+
+        public InvoiceController(InvoiceDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Invoice
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Invoice>>> GetInvoices()
+        {
+            return await _context.Invoices.ToListAsync();
+        }
+
+        // GET: api/Invoice/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Invoice>> GetInvoice(Guid id)
+        {
+            var invoice = await _context.Invoices.FindAsync(id);
+
+            if (invoice == null)
+            {
+                return NotFound();
+            }
+
+            return invoice;
+        }
+
+        // PUT: api/Invoice/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutInvoice(Guid id, Invoice invoice)
+        {
+            if (id != invoice.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(invoice).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!InvoiceExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Invoice
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Invoice>> PostInvoice(Invoice invoice)
+        {
+            _context.Invoices.Add(invoice);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetInvoice", new { id = invoice.Id }, invoice);
+        }
+
+        // DELETE: api/Invoice/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteInvoice(Guid id)
+        {
+            var invoice = await _context.Invoices.FindAsync(id);
+            if (invoice == null)
+            {
+                return NotFound();
+            }
+
+            _context.Invoices.Remove(invoice);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool InvoiceExists(Guid id)
+        {
+            return _context.Invoices.Any(e => e.Id == id);
+        }
+    }
+}
